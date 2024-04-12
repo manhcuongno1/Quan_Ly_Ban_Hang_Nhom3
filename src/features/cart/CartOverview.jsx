bde0226eefc76462fd58d6be6955328758937051
manhcuongno1 <cuongnguyen712017@gmail.com>
2024-04-12 08:55:56 +0700

    init project

---
+++ src/features/cart/CartOverview.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function CartOverview() {
+  return (
+    <div className="flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
+      <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6"></p>
+      <Link to="/cart">Open cart &rarr;</Link>
+    </div>
+  );
+}
+
+export default CartOverview;
